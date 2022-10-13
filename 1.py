af4947c1328ef3af87a40273ece725a897c1c582
MatveyFilippov <mdfilippov_2@edu.hse.ru>
2022-10-13 19:23:24 +0300

    6 commit

--- 1.py
+++ 1.py
@@ -1,17 +1,17 @@
 input("Чтобы начать нажмите ENTER")
 print()
-SIZE= int(input("Введите размер масива: "))
+SIZE = int(input("Введите размер масива: "))
 ARRAY = [0] * SIZE
 print()
 print("Выберите  K  если хотите ввести элементы с клавиатуры" )
 print("Выберите  R  если хотите, чтоб элементы были сгенерированы рандомно в диапозоне от -999999 до 999999" )
 CHOICE = input("Введите K или R: ")
 while CHOICE != "K" and CHOICE != "R":
-    CHOICE = input("Ошибка! Введите заглавные Y или R: ")
+    CHOICE = input("Ошибка! Введите заглавные K или R: ")
 else:
-    print("Хорошо!")
-    print()
     if CHOICE == "K":
+        print("Хорошо!")
+        print()
         for i in range(SIZE):
             print("Введите элемент №", i + 1,":", sep="", end=" ")
             ARRAY[i] = float(input())
@@ -19,11 +19,18 @@ else:
         from random import randint
         for i in range(SIZE):
             ARRAY[i] = randint(-999999, 999999)
-print()
-input("Нажмите ENTER чтобы проверить верность полученных данных")
+print("Принято! Сверьтесь всё ли верно?")
 print()
 print("Размера массива:", SIZE)
 print("Ваш массив:", ARRAY)
 print()
-input("Если всё верно нажмите ENTER")
+input("Нажмите ENTER")
+IMAX = 0
+for i in range(SIZE):
+    if ARRAY[i] > ARRAY[IMAX]:
+        IMAX = i
+
+ARRAY[IMAX + 1: len(ARRAY)] = [0] * (SIZE - 1 - IMAX)
+print("Полученный массив:", ARRAY)
 print()
+input("Чтобы завершить нажмите ENTER")
